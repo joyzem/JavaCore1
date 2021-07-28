cdac5b2c24fc2447f2ddb9a67df7533313a4144b
Rodion Gazizov <rodion.gazizov@bk.ru>
2021-07-28 16:55:47 +0300

    First task is done

---
+++ app/src/main/java/com/example/user/javacoretraining/javaPart2/Task1.java
@@ -0,0 +1,42 @@
+/*
+ @author Rodion Gazizov
+ */
+
+package com.example.user.javacoretraining.javaPart2;
+
+/**
+ * Practice task number 1
+ *
+ * @version 1.0 28 Jul 2021
+ * @author Rodion Gazizov
+ */
+public class Task1 {
+    /* Class has repeatTask method that is called in main method. */
+
+    /** Main method of the class */
+    public static void main(String[] args) {
+        Printing printer = () -> System.out.println("I love Java");
+        repeatTask(10, printer);
+    }
+
+    /**
+     * This method runs runnable task certain amount of times.
+     * @param times - amount of repeats
+     * @param task - runnable task - lambda-expression
+     */
+    public static void repeatTask(int times, Runnable task) {
+        for (int i = 0; i < times; i++) {
+            task.run();
+        }
+    }
+
+}
+
+/**
+ * This is a functionality interface for implementing
+ * lambda-expression
+ */
+interface Printing extends Runnable {
+    @Override
+    void run();
+}
